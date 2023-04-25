f4af3d37949d5a7c01986cadd644107a78ed042c
Nati88 <113054513+Nati88@users.noreply.github.com>
2023-04-24 23:50:30 -0300

    Add files via upload
    Uso de with, archivos y contexto Manager

---
+++ Python/Semana02/ManejoArchivos.py
@@ -0,0 +1,9 @@
+class ManejoArchivos:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def __enter__(self):
+        print('Obtenemos el recurso'.center(50, '-'))
+        self.nombre = open(self.nombre, 'r', encoding='utf8') #Encapsulamos el código dentro del método
+        return self.nombre
+  
