a41e0eecfe53a7ed4755a7058186128ec0cf6773
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-04-25 18:48:59 -0300

    Update ManejoArchivos.py

--- Python/Semana02/ManejoArchivos.py
+++ Python/Semana02/ManejoArchivos.py
@@ -6,4 +6,8 @@ class ManejoArchivos:
         print('Obtenemos el recurso'.center(50, '-'))
         self.nombre = open(self.nombre, 'r', encoding='utf8') #Encapsulamos el código dentro del método
         return self.nombre
+    def __exit__(self, tipo_exception, valor_exception, traza_error):
+        print_('Cerramos el recurso'.center(50, '-'))
+        if self.nombre:
+            self.nombre.close() #cerramos el archivo
   
