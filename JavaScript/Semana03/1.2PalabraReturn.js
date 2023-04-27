c264cc0f35b2a461a69f8b3e9c442c4e857c68f1
nicoocastro <113069134+nicoocastro@users.noreply.github.com>
2023-04-27 16:06:53 -0300

    Palabra Return 1.2

---
+++ JavaScript/Semana03/1.2PalabraReturn.js
@@ -0,0 +1,12 @@
+miFuncion(8,2); //Esto se le conoce como hosting
+
+function miFuncion(a, b){
+    //console.log("Sumamos: "+ (a+b));
+    return a + b;
+}
+
+//Llamamos la función
+miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7,);
+console.log(resultado)
