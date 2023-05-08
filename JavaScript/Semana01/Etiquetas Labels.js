fbbdad4016212e10969a4e9aa80710d0f8dab1e0
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-05-08 18:25:49 -0300

    Add files via upload

---
+++ JavaScript/Semana01/Etiquetas Labels.js
@@ -0,0 +1,9 @@
+// La palabra continue y Etiquetas Labels
+inicio:
+for(Let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        break inicio; //ir a la siguiente interacción
+    }
+    console.log(contando); 0
+}
+console.log("Termina el ciclo"); Termina el ciclo
