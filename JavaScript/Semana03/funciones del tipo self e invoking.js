0172d2e40a38f8a8920abdf42ab97e0b9f267a99
Nati88 <113054513+Nati88@users.noreply.github.com>
2023-05-08 11:49:49 -0300

    Agregamos el video 1.5

--- JavaScript/Semana03/funciones del tipo self e invoking.js
+++ JavaScript/Semana03/funciones del tipo self e invoking.js
@@ -2,3 +2,13 @@
 (function(a,b){
     console.log('Ejecutando la funcion: '+(a+b));
 })(9, 6);
+
+console.log(typeof miFuncion);
+function miFuncionDos(a, b){
+    console.log(arguments.length);
+}
+miFuncionDos(5, 7, 3, 6);
+
+// toString
+var miFuncionTexto = miFuncionDos.toString();
+console.log(miFuncionTexto); 
