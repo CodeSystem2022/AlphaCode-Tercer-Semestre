5d2713fa9d5efb0c67856df56ccbf0f950d7e534
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-05-08 19:51:29 -0300

    Add files via upload

---
+++ Java/Semana03/TestAutoboxingUnboxing.java
@@ -0,0 +1,15 @@
+package test
+public class TestAutoboxingUnboxing {
+    public static void main(String args[]) {
+      //Clases envolventes o Wraper
+      /*
+        Clases envolventes de tipos primitivos
+        int = la clase envolvente es Integer
+       /*
+       
+      int enteroPrim = 10; //Tipo Primitivo
+      System.out.println("enteroPrim = " + enteroPrim);
+      Integer entero = 10; //Tipo object con la clase Integer
+      System.out.println("entero = " + entero);
+    }
+}
