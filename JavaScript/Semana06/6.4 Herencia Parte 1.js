c3e15149e655965304b6a80e73475062bdc5e03b
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-05-28 20:08:01 -0300

    Clase Padre

---
+++ JavaScript/Semana06/6.4 Herencia Parte 1.js
@@ -0,0 +1,22 @@
+class Persona{ //Clase Padre
+    constructor(nombre, apellido){
+    this._nombre = nombre;
+    this._apellido = apellido;
+}
+
+get nombre(){
+    return this._nombre;
+}
+set nombre(nombre){
+    this._nombre = nombre;
+}
+
+set nombre(nombre){
+    this._nombre = nombre;
+}
+set apellido(apellido){
+    this._apellido = apellido
+}
+
+}
+
