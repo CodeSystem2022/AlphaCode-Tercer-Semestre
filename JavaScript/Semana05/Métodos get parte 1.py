bc8103821fe6bad21ebe1e16c219590fcacce01d
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-05-28 18:44:22 -0300

    Métodos get parte 1.py

---
+++ JavaScript/Semana05/Métodos get parte 1.py
@@ -0,0 +1,4 @@
+ get nombreEdad(){ //Metodo get
+        return 'El nombre es: '+this.nombre+', edad: '+this.edad;
+ El nombre es: Carlos, Edad: 30
+console.log(persona.nombreEdad); El nombre es: Carlos, Edad: 30
