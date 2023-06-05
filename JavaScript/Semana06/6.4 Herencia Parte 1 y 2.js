75eefba4f35c6d110d2d3d0dac190b26f5e7f8fb
DaniAlessio <113067286+DaniAlessio@users.noreply.github.com>
2023-06-05 09:19:25 -0300

    Update 6.4 Herencia Parte 1 y 2.js

--- JavaScript/Semana06/6.4 Herencia Parte 1 y 2.js
+++ JavaScript/Semana06/6.4 Herencia Parte 1 y 2.js
@@ -53,4 +53,39 @@ let empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
 console.log(empleado1); Empleado { _nombre: 'Maria' , _apellido: 'Gimenez', _departamento 'Sistemas' }
 console.log(empleado1.nombre); Maria
 
+//Object.prototype.toString Esta es la manera de acceder a atributos y metodos de manera dinamica
+console.log(empleado1.toString());
+consoloe.log(persona1.toString());
+
+//persona1.saludar(); no se utiliza desde el objeto 
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1); 
+
+//console.logg(persona1.contadorObjetosPersona);
+consolo.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email); No puede acceder desde la clase 
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
+let persona3 = new Persona('Carla', 'Pertosi');
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
+
+console.log(Persona.MAX_OBJ);
+// Persona.MAX_ONJ = 10; // No se puedo modificar ni alterar 
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona('Franco', 'Diaz');
+console.log(persona4.toString());
+let persona5 = new Persona('Liliana', 'Paz');
+console.log(persona5.toString());
+
 
