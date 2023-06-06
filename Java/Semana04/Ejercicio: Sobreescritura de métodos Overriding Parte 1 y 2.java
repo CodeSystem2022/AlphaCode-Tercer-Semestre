2296e9de6f70556794831bc5eb0038d1b29da11d
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-06-06 00:44:38 -0300

    Ejercicio: Sobreescritura de métodos Overriding Parte 1 y 2.java

---
+++ Java/Semana04/Ejercicio: Sobreescritura de métodos Overriding Parte 1 y 2.java
@@ -0,0 +1,16 @@
+package domain;
+public class Gerente extends Empleado {
+  private String departamento;
+  public Gerente (String nombre, double sueldo, String departamento
+      super(nombre, sueldo);
+      this.departamento = departamento;
+                  
+                  
+                  
+
+package test;
+public class TestSobreescritura {
+Navigator public static void main(String[] args) {
+Gerente gerentel = new Gerente ("José", 5000, "Sistemas")
+System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+}
