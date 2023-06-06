c6a1e941849a5ea75dad65b0d4558dc1e62e680f
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-06-06 00:40:01 -0300

    Gerente.java

---
+++ Java/Semana04/Gerente.java
@@ -0,0 +1,6 @@
+package domain;
+public class Gerente extends Empleado {
+  private String departamento;
+  public Gerente (String nombre, double sueldo, String departamento
+      super(nombre, sueldo);
+      this.departamento = departamento;
