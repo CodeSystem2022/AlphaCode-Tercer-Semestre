9cbaea77b04710ba8f2ca997405a6e4766cc3b94
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-06-09 16:07:57 -0300

    OperacionExcepcion.java

--- Java/Leccion08/ManejoExcepciones/OperacionExcepcion.java
+++ Java/Leccion08/ManejoExcepciones/OperacionExcepcion.java
@@ -1,9 +1,9 @@
 
 package excepciones;
 
-public class OperacionExcepcion extends Exception{
+public class OperacionExcepcion extends Exception {
     //Constructor
-    public OperacionExcepcion(String mensaje){
+    public OperacionExcepcion(String mensaje) {
         super(mensaje);
     }
 }
