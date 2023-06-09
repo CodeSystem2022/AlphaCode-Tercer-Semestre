cfdfdd54c1d8cd1df3684ed6d725b9cc49466b52
nicoocastro <113069134+nicoocastro@users.noreply.github.com>
2023-06-08 22:47:32 -0300

    añadimos el archivo de semana 2

--- JavaScript/TSLección02/02-03-arreglos.js
+++ JavaScript/TSLección02/02-03-arreglos.js
@@ -11,6 +11,7 @@ console.log(autos[2]);
 for(let i = 0; i < autos.length; i++){
     console.log(i+' : '+autos[i]);
 }
+
 // Modificamos los elementos del arreglo
 autos[1] = 'Volvo';
 console.log(autos[1]);
@@ -18,3 +19,17 @@ console.log(autos[1]);
 // Agregamos nuevos valores al arrelgo
 autos.push('Audi'); // Agregamos el elemento al final del arreglo
 console.log(autos);
+
+//Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porche';
+console.log(autos);
+
+//Tercera forma de agregar elementos teniendo CUIDADO
+autos[6] = 'Renault';
+console.log(autos);
+
+//Como preguntar si es un array o arreglo
+console.log(Array.isArray(autos)); //Devuelve un booleano
+
+
+console.log(autos instanceof Array); //Preguntamos si la variable es una instancia de la clase Array
