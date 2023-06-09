805110291522448ca539ba39ba6b967c82b3be1b
Nati88 <113054513+Nati88@users.noreply.github.com>
2023-06-08 22:57:36 -0300

    Completamos funciones

--- JavaScript/TSLeccion03/01-03-funciones.js
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -37,11 +37,49 @@ const sumarFuncionFlecha = (a, b) => a + b;
 resultado = sumarFuncionFlecha(3, 7);//Asignamos el valor a una variable
 console.log(resultado);
 
+//Función tipo expresión
 let sumar = function(a = 4, b = 8){
-    console.log (arguments[0]); //muestra el parametro de: a
-    console.log (arguments[1]); //muestra el parametro de: b
-    return a + b + arguments [2]; 
+    console.log(arguments[0]); //Muestra el parámetro de: a
+    console.log(arguments[1]); //muestra el parámetro de: b
+    return a + b + arguments[2];
 }
+resultado = sumar(3, 2, 9);
+console.log(resultado);
+
+//Sumar todos los argumentos
+let respuesta = sumarTodo(5, 4, 13, 10, 9);
+console.log(respuesta);
+function sumarTodo(){
+    let suma = 0;
+    for(let i = 0; i < arguments.length; i++){
+        suma += arguments[i]; //arguments es para arreglos
+    }
+    return suma;
+}
+
+//Tipos primitivos
+let k = 10;
+function cambiarValor(a){ //Paso por valor
+    a = 20;
+}
+cambiarValor(k);
+console.log(k);
+
+//Paso por referencia
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Lepez'
+}
+
+console.log(persona);
+function cambiarValorObjeto(p1){
+    p1.nombre = 'Ignacio';
+    p1.apellido = 'Perez';
+}
+
+cambiarValorObjeto(persona);
+console.log(persona);
+
 
 
 
