96dc20398fa0620fd7a4caf108e4cf24d773e86e
nicoocastro <113069134+nicoocastro@users.noreply.github.com>
2023-06-08 22:28:19 -0300

    Update 01-04-objetos.js

--- JavaScript/TSLección4/01-04-objetos.js
+++ JavaScript/TSLección4/01-04-objetos.js
@@ -9,3 +9,26 @@ Let persona = {
     edad: 30
 }
 console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
+console.log(persona);
+console.log(persona.nombreCompleto());
+console.log('Ejecutando con un objeto');
+let persona2 = new Object(); //Debe crear un nuevo objeto en memoria
+persona2.nombre = 'Juan';
+persona2.direccion = 'Salada 14';
+persona2.telefono = '543849384938'
+console.log(persona2.telefono);
+console.log('Creamos un nuevo objeto');
+console.log(persona['apellido']); //accedemos con si fuera un arreglo
+console.log('Usamos el ciclo for in');
+//for in y accedemos al objeto como si fuera un arreglo
+for(propiedad in persona){
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+}
+console.log('Cambiamos y eliminamos un error');
+persona.apellida = 'Castro'; //Cambiamos dinamicamente un valor del objeto
+delete persona.apellida; //Eliminamos el error 
+console.log(persona);
