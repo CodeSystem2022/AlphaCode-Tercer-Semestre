0ce4b79ce9035840af698ccf0dab5866b9a4d562
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-06-09 14:01:05 -0300

    File

---
+++ Python/Lección08/Prueba de la Clase Persona.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__":
+    persona1 = Persona(1, 'Juan', 'Perez', 'jperez@mail.com')
+    log.debug(persona1)
+    persona2 = Persona(nombre='Jose', apellido='Lepez', email='ljose@mail.com')
