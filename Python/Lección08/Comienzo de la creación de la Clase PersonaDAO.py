dd40f97b20b1688cfe4f830ec38a7f41599bcdd8
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-06-09 13:49:09 -0300

    .

---
+++ Python/Lección08/Comienzo de la creación de la Clase PersonaDAO.py
@@ -0,0 +1,9 @@
+class PersonaDAO:
+    """
+    DAO significa: Data Access Object
+    CRUD significa:
+                    Create -> Insertar
+                    Read   -> Seleccionar
+                    Uptade -> Actualizar
+                    Delete -> Eliminar
+    """
