ab3d8420c667306a481cd0a74267dfbd04bf0f02
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-06-11 10:59:49 -0300

    Add files via upload

--- Python/Leccion04/BD/prueba_bd.py
+++ Python/Leccion04/BD/prueba_bd.py
@@ -2,9 +2,16 @@ import psycopg2
 
 conexion = psycopg2.connect(
     user='postgres',
-    password='admin',
+    password = 'admin',
     host='127.0.0.1',
     port='5432',
     database='test_bd'
 )
-print(conexion)
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
+registros = cursor.fetchall() # Recuperamos todos los registros que serian una lista
+print(registros)
+
+cursor.close()
+conexion.close()
