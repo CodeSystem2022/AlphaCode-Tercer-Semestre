14c7a197ae8d1556397280e47222b90a67fcac72
Nati88 <113054513+Nati88@users.noreply.github.com>
2023-06-26 21:13:21 -0300

    Agregamos la clase empleado

---
+++ JavaScript/TSLeccion07(Clase09)/Empleado.js
@@ -0,0 +1,28 @@
+class Empleaños extends Persona{
+
+    static contadorEmpleados = 0;
+    constructor(nombre, apellido, edad, sueldo){
+        super(nombre, apellido, edad);
+        this._idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    get _idEmpleado(){
+        return this._idEmpleado;
+    }
+
+    get sueldo(){
+        this.sueldo;
+    }
+
+    set sueldo(sueldo){
+        this.sueldo = sueldo;
+    }
+
+    toString(){
+        return `
+        ${super.toString()} 
+        ${this._idEmpleado} 
+        ${this._sueldo}`;
+    }
+}
