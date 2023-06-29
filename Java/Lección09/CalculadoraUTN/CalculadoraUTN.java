f2f53ecaed1d9beb61e5981641746c27f77a7349
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-06-28 23:01:23 -0300

    Update CalculadoraUTN.java

--- Java/Lección09/CalculadoraUTN/CalculadoraUTN.java
+++ Java/Lección09/CalculadoraUTN/CalculadoraUTN.java
@@ -1,6 +1,11 @@
 public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        while (true){ //Ciclo Infinito
+
+            
+        } //Fin while
         System.out.println("******* Aplicación Calculadora *******");
         System.out.print("Digite el valor para el operando1: ");
         var operando1 = Integer.parseInt(entrada.nextLine());
@@ -10,3 +15,39 @@ public class CalculadoraUTN {
         System.out.println("resultado = " + resultado);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        break; //Rompe el ciclo y sale
+
+        //Imprimimos un salto de Linea antes de repetir el menú
+       
+
+    System.out.println();
+         } //Fin while
