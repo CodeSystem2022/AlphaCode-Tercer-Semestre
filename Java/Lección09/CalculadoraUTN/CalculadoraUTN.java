d183c20c4b05179739ae4c9415d7380b76c45774
DaniAlessio <113067286+DaniAlessio@users.noreply.github.com>
2023-06-29 08:38:56 -0300

    CalculadoraUTN.java
    Agregamos Modificaciones al Codigo

--- Java/Lección09/CalculadoraUTN/CalculadoraUTN.java
+++ Java/Lección09/CalculadoraUTN/CalculadoraUTN.java
@@ -1,27 +1,35 @@
-public class CalculadoraUTN {
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-
-        while (true){ //Ciclo Infinito
-
-            
-        } //Fin while
-        System.out.println("******* Aplicación Calculadora *******");
-        System.out.print("Digite el valor para el operando1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Digite el valor para el operando1: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
-    }
-}
 
 import java.util.Scanner;
 public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        while (true) {
         System.out.println("******* Aplicacion Calculadora *******");
-        //Mostramos el menú
+        mostrarMenu();//Mostramos el menú
+          try {
+                var operacion = Integer.parseInt(entrada.nextLine());
+                if(operacion >= 1 && operacion <= 4){
+                    ejecutarOperacion(operacion, entrada);
+            } //Fin del if
+            else if (operacion == 5) {
+                System.out.println("Hasta pronto...");
+                break; //Rompe el ciclo y sale
+            }
+            else {
+                System.out.println("Opcion erronea: "+operacion);
+            }
+            // Imprimimos un salto de linea antes de repetir el menu
+            System.out.println();
+
+            } catch (Exception e){ //Fin del try, comienzo del catch
+                System.out.println("Ocurrio ubn error: "+e.getMessage());
+                System.out.println();
+            } 
+        } 
+    } 
+
+    private static void mostarMenu(){
+        //Mostramos el menu
         System.out.println("""
                 1. Suma
                 2. Resta
