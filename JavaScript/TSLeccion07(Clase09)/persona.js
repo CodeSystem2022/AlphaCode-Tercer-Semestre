3a83810d94dcb0379746a5d7aa8350af60fafe60
DaniAlessio <113067286+DaniAlessio@users.noreply.github.com>
2023-06-30 10:28:54 -0300

    persona.js

--- JavaScript/TSLeccion07(Clase09)/persona.js
+++ JavaScript/TSLeccion07(Clase09)/persona.js
@@ -4,7 +4,8 @@ class Persona{
                          
                             
     constructor(nombre, apellido, edad){
-    this._idPersona = +.             +Persona.contadorPersonas;
+    this._idPersona = +.            
+    +Persona.contadorPersonas;
     this._nombre = nombre;
     this._apellido = apellido;
     this._edad = edad;
@@ -16,22 +17,22 @@ class Persona{
     get nombre(){
      this._nombre;
 }
-  set nombre(nombre){
-    this._nombre = nombre;
+    set nombre(nombre){
+     this._nombre = nombre;
 
 }
 
-  get apellido(){
+   get apellido(){
     return this._apellido;
  }
 
-  set apellido(apellido){
+   set apellido(apellido){
     this._apellido = apellido;
  }
-  get edad(){
+   get edad(){
     return this._edad;
  }
-  set edad(edad){
+   set edad(edad){
     this._edad = edad;
  
  }
