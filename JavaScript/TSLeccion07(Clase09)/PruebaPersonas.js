39a40e9228fed9723dd6d3bd58c7e85476380ae4
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-06-30 12:44:43 -0300

    PruebaPersonas.js

--- JavaScript/TSLeccion07(Clase09)/PruebaPersonas.js
+++ JavaScript/TSLeccion07(Clase09)/PruebaPersonas.js
@@ -115,7 +115,7 @@ console.log(empleaedo2.toString());
 
 //Prueba clase Cliente
 let cliente1 = new Cliente('Miguel', 'Zala', 29, new Date());
-console.log(liente1.toString());
+console.log(cliente1.toString());
 
 let cliente2 = new Cliente('Natalia', 'Ortega', 22, new Date());
-console.log(liente2.toString());
+console.log(cliente2.toString());
