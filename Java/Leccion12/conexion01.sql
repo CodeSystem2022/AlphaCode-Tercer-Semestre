0673a8575b70e167926bfde2ba9a52f01d10cbff
Gimenap <113073790+Gimenap@users.noreply.github.com>
2023-07-04 15:51:50 -0300

    Add files via upload

---
+++ Java/Leccion12/conexion01.sql
@@ -0,0 +1,8 @@
+
+-- Comenzamos con CRUD: create(insertar), read(leer), update(actualizar), delete(eliminar)
+-- Listar los estudiantes (read)
+SELECT * FROM estudiantes2022;
+-- Insertar estudiante
+INSERT INTO estudiantes2022(nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "2614545456", "juan@mail.com");
+
+
