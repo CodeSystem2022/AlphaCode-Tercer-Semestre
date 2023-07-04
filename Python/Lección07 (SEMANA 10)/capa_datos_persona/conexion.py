b2c021e5a05089878f0832dffccb807cd3496fb4
DaniAlessio <113067286+DaniAlessio@users.noreply.github.com>
2023-07-03 21:22:28 -0300

    conexion.py
    Modificamos el archivo según clase numero 11

--- Python/Lección07 (SEMANA 10)/capa_datos_persona/conexion.py
+++ Python/Lección07 (SEMANA 10)/capa_datos_persona/conexion.py
@@ -26,8 +26,41 @@ class Conexion:
     def obtenerPool(cls):
         if cls._pool is None:
           try:
-              cls._pool = pool.SimpleConnectionPool()
+              cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                    cls._MAX_CON,
+                                                    host = cls._HOST,
+                                                    user = cls._USERNAME,
+                                                    password = cls._PASSWORD,
+                                                    port = cls._DB_PORT,
+                                                    database = cls._DATABASE)
+              log.debug(f'Creacion del pool exitosa: {cls._pool}')
+              return cls._pool
+          except Exception as e:
+              log.error(f'Ocurrio un error al obtener el pool: {e}')
+              sys.exit()
+        else:
+            return cls._pool
+
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f"Regresamos la conexion del pool: {conexion}")
+
+    @classmethod
+    def cerrarConexiones(cls):
+        cls.obtenerPool().closeall()
+
+                 
+
+if __name__ == "__main__":
+    conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
+    conexion2 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion2)
+    conexion3 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion3)
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+    Conexion6 = Conexion.obtenerConexion()
 
 
-if __name__ == '__main__':
-    pass
