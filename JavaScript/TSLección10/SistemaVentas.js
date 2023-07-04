909586cf30fe5d2694c50d3b2e9f4119f331e09c
DaniAlessio <113067286+DaniAlessio@users.noreply.github.com>
2023-07-03 21:42:40 -0300

    SistemaVentas.js
    completamos en archivo clase 10.3

--- JavaScript/TSLección10/SistemaVentas.js
+++ JavaScript/TSLección10/SistemaVentas.js
@@ -37,3 +37,56 @@ let producto1 = new Producto('Pantalón', 200);
 let producto2 = new Producto('Camisa', 150);
 console.log(producto1.toString());
 console.log(producto2.toString());
+
+
+class Orden{
+  static contadorOrdenes = 0;
+  static get_MAX_PRODUCTOS(){
+      return 5;
+  }
+
+  constructor(){
+      this._idOrden = ++Orden.contadorOrdenes;
+      this._productos = [];
+      this._contadorProductosAgregados = 0;
+  }
+
+  get idOrden(){
+      return this._idOrden;
+  }
+
+  agregarProducto(producto){
+      if(this._productos.length < Orden.get_MAX_PRODUCTOS){
+          this._productos.push(producto);
+      }else{
+          console.log('No se pueden agregar más productos');
+      }
+  }
+
+  calcularTotal(){
+      let totalVenta = 0;
+      for(let producto of this._productos){
+          totalVenta += producto.precio;
+      }
+      return totalVenta;
+  }
+
+  mostrarOrden(){
+      let productosOrden = '';
+      for(let producto of this._productos){
+          productosOrden += '\n{' + producto.toString() + '}';
+      }
+      console.log(`Orden: ${this._idOrden} Total: $${this.calcularTotal()}, Productos: ${productosOrden}`);
+  }
+
+} //Fin de la clase Orden
+
+let producto1 = new Producto('Pantalon', 200);
+let producto2 = new Producto('Camisa', 100);
+let producto3 = new Producto('Cinturon', 50);
+let orden1 = new Orden();
+let orden2 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden1.agregarProducto(producto3);
+orden1.mostrarOrden();
