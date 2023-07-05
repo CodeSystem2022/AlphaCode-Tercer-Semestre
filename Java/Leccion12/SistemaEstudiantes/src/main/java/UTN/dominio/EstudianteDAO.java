cd48fc09616f13d748a728dfaf07983c1fa6f29a
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-07-04 23:54:37 -0300

    Update EstudianteDAO.java

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/dominio/EstudianteDAO.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/dominio/EstudianteDAO.java
@@ -1,7 +1,7 @@
 
 import UTN.dominio.Estudiante;
 
-
+import static UTN.conexion.Conexion.getConnection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -14,8 +14,21 @@ public class EstudianteDAO{
     //Creamos ahora algunos objetos que son necesarios para comunicarnos con la base de datos
     PreparedStatement ps; //Envia la sentencia de la base de datos
     ResultSet rs; //Obtenemos el resultado de la base de datos
-  }
+    //Creamos un objeto de tipo conexion
+    Connection con = getConnection();
+    String sql = "SELECT * FROM estudiantes2022 ORDER BY estudiantes2022";
+    try{
+        ps = con.prepareStatement(sql);
+        rs = ps.executeQuery();
+      while(rs.next()){
+        var estudiante = new Estudiante();
+        estudiante.setIdEstudiante(rs.getInt("idestudiante2022");
+        
+      }
+    }catch (Exception e){
+      System.out.print("Ocurrio un error al seleccionar datos: "+e.getMessage());
 }
+  
 
   
 
