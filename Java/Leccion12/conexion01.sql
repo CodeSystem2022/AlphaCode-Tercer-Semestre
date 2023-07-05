1f74d7609226997915574e3394607ff562607e32
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-07-05 11:28:49 -0300

    conexion01.sql

--- Java/Leccion12/conexion01.sql
+++ Java/Leccion12/conexion01.sql
@@ -4,9 +4,9 @@
 SELECT * FROM estudiantes2022;
 -- Insertar estudiante
 INSERT INTO estudiantes2022(nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "2614545456", "juan@mail.com");
---Update (modificar)
+-- Update (modificar)
 UPDATE estudiantes2022 SET nombre="Juan Carlos", apellido="Garcia" WHERE idestudiantes2022= 1;
---Delete(eliminar)
+-- Delete(eliminar)
 DELETE FROM estudiantes2022 WHERE idestudiantes2022=3;
---Para modificar el idestudiantes2022 y comience en 1
+-- Para modificar el idestudiantes2022 y comience en 1
 ALTER TABLE estudiantes2022 AUTO_INCREMENT = 1;
