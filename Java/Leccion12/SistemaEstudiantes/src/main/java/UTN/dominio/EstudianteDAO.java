1c00d8e5f1edaad18234d89357da3feb6d7d1002
nicoocastro <113069134+nicoocastro@users.noreply.github.com>
2023-07-05 07:45:22 -0300

    Update EstudianteDAO.java

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/dominio/EstudianteDAO.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/dominio/EstudianteDAO.java
@@ -104,8 +104,33 @@ public class EstudianteDAO{
         }//Fin metodo agregarEstudiante
 
     }
-
-
+      //12.8 Agregamos el método -> modificarEstudiante()
+public boolean modificar estudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "UPDATE estudiantes2022 SET nombre=?, apellido=?, telefono=?, eimail=? WHERE idestudiantes2022=?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.setInt(5, estudiante.getIdEstudiante());
+            ps.execute();
+            return true
+        } catch (Exception e){
+            System.out.println("Eror al modificar estudiante: "+e.getMessage());
+        }//Fin catch
+        finally {
+            try{
+                con.close();
+            } catch (Exception e){
+                System.out.println("Error al cerrar la conexión: "+e.getMessage());
+            }//FIn catch
+        }//Fin finally
+        return false;
+    }//Fin método modificar estudiante
+      //Fin 12.8 Agregamos el método -> modificarEstudiante()
 
 
   }
