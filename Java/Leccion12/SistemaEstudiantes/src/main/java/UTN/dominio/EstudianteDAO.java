cda191b88420c74a56638dd4a9afd56ef8bfce0f
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-07-04 22:58:20 -0300

    Create EstudianteDAO.java

---
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/dominio/EstudianteDAO.java
@@ -0,0 +1,22 @@
+
+import UTN.dominio.Estudiante;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudianteDAO{
+  //Metod listar
+  public List<Estudiante> listar(){
+    List<Estudiante> estudiantes = new ArrayList<>();
+    //Creamos ahora algunos objetos que son necesarios para comunicarnos con la base de datos
+    PreparedStatement ps; //Envia la sentencia de la base de datos
+    ResultSet rs; //Obtenemos el resultado de la base de datos
+  }
+}
+
+  
+
+
